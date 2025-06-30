26cb8c381aa6e4c8a9ab770d42817085607d24da
Vadym Kozachuk <77024298+KVadym-del@users.noreply.github.com>
2025-06-30 21:58:48 +0200

    Fixed premake5 file.

--- premake5.lua
+++ premake5.lua
@@ -1,32 +1,14 @@
-workspace "D3D12MemoryAllocator"
-   startproject "D3D12MemoryAllocator"
-   configurations { "Debug", "Release" }
-   platforms      { "x86", "x64" }
-
-filter "platforms:x86"
-   architecture "x86"
-filter "platforms:x64"
-   architecture "x86_64"
-
-   language    "C++"
-   cppdialect  "C++17"
-   staticruntime "On"
-   systemversion "latest"
-   targetdir   "bin/%{cfg.buildcfg}_%{cfg.platform}"
-   objdir      "bin-int/%{cfg.buildcfg}_%{cfg.platform}"
-
-filter "configurations:Debug"
-   runtime "Debug"
-   symbols "On"
-filter "configurations:Release"
-   runtime "Release"
-   optimize "On"
-
-filter {}
-
-
 project "D3D12MemoryAllocator"
-   kind "None"
+   kind "StaticLib"
+   language "C++"
+   cppdialect "C++17"
 
-   files { "D3D12MemAlloc.h" }
-   includedirs { "." }
+   targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+   objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+   includedirs { "%{prj.location}/include" }
+
+   files {
+      "%{prj.location}/include/D3D12MemAlloc.h",
+      "%{prj.location}/src/D3D12MemAlloc.cpp"
+   }
